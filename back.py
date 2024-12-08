e227eeb8bd9cde543b21877818ce331dab213fbe
Ixetan <foker11163@gmail.com>
2024-12-08 15:42:41 +0800

    -

---
+++ back.py
@@ -0,0 +1,16 @@
+from PIL import Image,ImageFilter
+
+
+def open_image(filename):
+    return Image.open(filename)
+
+
+def blur_image(filename):
+    image = Image.open(filename)
+    image = image.filter(ImageFilter.GaussianBlur(2))
+    image.save(filename)
+    
+def unmask_image(filename):
+    image = Image.open(filename)
+    image = image.filter(ImageFilter.UnsharpMask())
+    image.save(filename)
